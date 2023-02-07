faa1a0e6227fec4757872d94ff07ca3556cdccfb
Geuni <dlrmsgnl620@naver.com>
2023-02-07 22:13:20 +0900

    18. 버튼과 체크박스의 초기 조건 테스트

--- src/App.test.js
+++ src/App.test.js
@@ -20,3 +20,15 @@ test("button has correct initial color, and updates when clicked", () => {
 
   expect(colorButton).toHaveTextContent("Change to red");
 });
+
+test("initial conditions", () => {
+  render(<App />);
+
+  // check that the button starts out enabled
+  const colorButton = screen.getByRole("button", {name: "Change to blue"});
+  expect(colorButton).toBeEnabled();
+
+  // check that the checkbox starts out unchecked
+  const checkbox = screen.getByRole("checkbox");
+  expect(checkbox).not.toBeChecked();
+});
