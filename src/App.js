80ae0fc4586b3ac720eca3c2e5854e03885804ad
Geuni <dlrmsgnl620@naver.com>
2023-02-07 22:00:32 +0900

    16. 선택적 React코드: 버튼을 클릭하여 색상 변경하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import {useState} from "react";
 
 function App() {
+  const [buttonColor, setButtonColor] = useState("red");
+  const newButtonColor = buttonColor === "red" ? "blue" : "red";
+
   return (
     <div>
-      <button style={{backgroundColor: "red"}}>Change to blue</button>
+      <button
+        onClick={() => setButtonColor(newButtonColor)}
+        style={{backgroundColor: buttonColor}}
+      >
+        Change to {newButtonColor}
+      </button>
     </div>
   );
 }
